de972bc88e6fe8a7ccdbb8f8459cc9fd2820ecba
George <george.bullock@gmx.de>
2020-03-14 01:05:29 +0100

    RVB-003: Add react testing depencencies. Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
 					options: {
 						presets: [
 							'@babel/preset-env',
+							'@babel/preset-react',
 							{
 								plugins: ['@babel/plugin-proposal-class-properties']
 							}
